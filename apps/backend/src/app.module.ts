e2f289df6f8f52fe489fca77be60a5fd4d4a237a
Nilton Naab <nilton.naab@gmail.com>
2025-11-24 16:36:54 -0400

    Implement initial Auth, Documents, Employee, and Users modules with services, controllers, DTOs, entities, and tests.

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { EmployeeModule } from './employee/employee.module';
+import { UsersModule } from './users/users.module';
+import { DocumentsModule } from './documents/documents.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, EmployeeModule, UsersModule, DocumentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
